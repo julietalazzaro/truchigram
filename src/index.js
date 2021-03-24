830c64216d65d8c98f1e9872ee7d405cc5516a45
Julieta Lazzaro <julietalazzaro@gmail.com>
2021-03-24 08:50:00 -0300

    Pagina Truchigram

--- src/index.js
+++ src/index.js
@@ -5,15 +5,27 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import firebase from "firebase/app";
 
+// firebase.initializeApp({
+//   apiKey: "AIzaSyAyXxQijG4eoHvsHEPyPCJTi26roSRXupc",
+//   authDomain: "truchigram.firebaseapp.com",
+//   databaseURL: "https://truchigram.firebaseio.com",
+//   projectId: "truchigram",
+//   storageBucket: "truchigram.appspot.com",
+//   messagingSenderId: "887923179137",
+//   appId: "1:887923179137:web:b8ddab7c7f929486"
+// });
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 firebase.initializeApp({
-  apiKey: "AIzaSyAyXxQijG4eoHvsHEPyPCJTi26roSRXupc",
-  authDomain: "truchigram.firebaseapp.com",
-  databaseURL: "https://truchigram.firebaseio.com",
-  projectId: "truchigram",
-  storageBucket: "truchigram.appspot.com",
-  messagingSenderId: "887923179137",
-  appId: "1:887923179137:web:b8ddab7c7f929486"
-});
+  apiKey: "AIzaSyBaI0ruU8YBoXhGQ-Xsz46jfFzfl06kACk",
+  authDomain: "truchigram-julieta.firebaseapp.com",
+  databaseURL: "https://truchigram-julieta-default-rtdb.firebaseio.com",
+  projectId: "truchigram-julieta",
+  storageBucket: "truchigram-julieta.appspot.com",
+  messagingSenderId: "556206234462",
+  appId: "1:556206234462:web:e56b09bb299d46cd990dcf",
+  measurementId: "G-4PRJEHBSD5"
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
